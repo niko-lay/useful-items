035d5874b3c9e41762ae31d050fb284f1752804f
Nikolay <djan.mail@gmail.com>
2017-02-13 13:58:51 +0200

    can specify number of split parts

--- bin/split.sh
+++ bin/split.sh
@@ -4,9 +4,8 @@
 SPLIT_COUNT=7
 
 
-
 if [ -z ${1+x} ]; then
-echo "Filename not provided. Usage: '$0 filename'";
+echo "Filename not provided. Usage: '$0 filename [count=7]'";
 exit 1
 fi
 
@@ -15,10 +14,22 @@ echo "File not exists";
 exit 2
 fi
 
+if [ ! -z ${2+x} ]; then
+  re='^[0-9]+$'
+  if ! [[ $2 =~ $re ]] ; then
+    echo "error: '$2' not a number" >&2;
+    exit 1
+  else
+   SPLIT_COUNT=$2
+  fi
+fi
+
 TOTAL_LINES=`cat $1 | wc -l`
 echo "Tolal lines: ${TOTAL_LINES}";
+echo "Split into: ${SPLIT_COUNT} parts"
 LINES_PER_ITEM=$(( TOTAL_LINES / SPLIT_COUNT ))
 echo "Lines per item: ${LINES_PER_ITEM}";
+echo ""
 
 for (( c=1; c<=$SPLIT_COUNT; c++ ))
 do
